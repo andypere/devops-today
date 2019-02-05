faff583438f9280c5d5aab64302ef9e14aba5ba5
Andy Perez <andypere01@gmail.com>
2019-02-05 14:08:22 -0500

    add tests project to solution

---
+++ DevOpsToday.MSTesting/Tests/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+using DevOpsToday.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace DevOpsToday.MSTesting.Tests
+{
+  [TestClass]
+
+  public class HomeControllerTests
+  {
+    [TestMethod]
+
+    public void Test_IndexAction()
+    {
+      var sut=new HomeController();
+      Assert.IsNotNull(sut.Index());
+
+    }
+
+    public void Test_PrivacyAction()
+    {
+      var sut=new HomeController();
+      Assert.IsNotNull(sut.Privacy());
+    }
+  }
+}
